7b4da843c986e180ad3f193339f878e5e8291024
MAGIC-AAMAS <75066822+MAGIC-AAMAS@users.noreply.github.com>
2021-02-08 21:32:32 -0500

    Update plot.py

--- plot.py
+++ plot.py
@@ -5,17 +5,15 @@ import numpy as np
 import sys
 import glob
 
+plt.rcParams.update({'font.size': 20})
+
 colors_map = {
-    'ic3net': '#dc8d6d',
-    'commnet': '#5785c1',
-    'maddpg': '#78b38a',
-    'maac': '#ba71af',
-    'gc_ic3net': '#ff6373',
-    'gc_commnet': '#dec718',
-    'tar_ic3net': '#9d62c4',
-    'tar_commnet': '#62c4b9',
-    'gacomm': '#77ab3f',
-    'other': '#fa9e25'
+    'IC3Net': '#fca503',
+    'CommNet': '#b0b0b0',
+    'TarMAC-IC3Net': '#b700ff',
+    'GA-Comm': '#77ab3f',
+    'MAGIC (Our Approach)': '#0040ff',
+    'MAGIC w/o the Scheduler': '#ff6373'
 }
 
 def read_file(vec, file_name, term):
@@ -36,6 +34,8 @@ def read_file(vec, file_name, term):
                 epoch_line = lines[epoch_idx]
 
             epoch = int(epoch_line.split(' ')[1].split('\t')[0])
+            if file_name == 'log_files/tj_medium/commnet_tj_medium_no_cur_run1.log':
+                epoch -= 4000
 
             floats = line.split('\t')[0]
             left_bracket = floats.find('[')
@@ -64,33 +64,31 @@ def parse_plot(files, term='Reward'):
     episode_coll = dict()
     for fname in files:
         f = fname.split('.')
-        if 'ic3net' in fname and not 'gc' in fname and not 'tar' in fname:
-            label = 'ic3net'
-        elif 'commnet' in fname and not 'gc' in fname and not 'tar' in fname:
-            label = 'commnet'
-        elif 'maac' in fname:
-            label = 'maac'
-        elif 'maddpg' in fname:
-            label = 'maddpg'
-        elif 'gc_ic3net' in fname:
-            label = 'gc_ic3net'
-        elif 'gc_commnet' in fname:
-            label = 'gc_commnet'
+        if 'ic3net' in fname and not 'tar' in fname:
+            label = 'IC3Net'
+        elif 'commnet' in fname:
+            label = 'CommNet'
         elif 'tar_ic3net' in fname:
-            label = 'tar_ic3net'
-        elif 'tar_commnet' in fname:
-            label = 'tar_commnet'
+            label = 'TarMAC-IC3Net'
         elif 'gacomm' in fname:
-            label = 'gacomm'
-        else:
-            label = 'other'
+            label = 'GA-Comm'
+        elif 'gcomm' in fname and not 'complete' in fname:
+            label = 'MAGIC (Our Approach)'
+        elif 'gcomm' in fname and 'complete' in fname:
+            label = 'MAGIC w/o the Scheduler'
 
         if label not in coll:
             coll[label] = []
             episode_coll[label] = []
 
+        if 'ic3net_pp_hard' in fname and not 'tar' in fname and term == 'Steps-Taken':
+        	term = 'Steps-taken'
+
         coll[label] = read_file(coll[label], fname, term)
         episode_coll[label] = read_file(episode_coll[label], fname, 'Episode')
+
+        if 'ic3net_pp_hard' in fname and not 'tar' in fname and term == 'Steps-taken':
+        	term = 'Steps-Taken'
 
     for label in coll.keys():
         coll[label] = coll[label][:1000]
@@ -106,7 +104,7 @@ def parse_plot(files, term='Reward'):
             if term == 'Success':
                 mean *= 100
             mean_values.append(mean)
-            variance = np.std(val)
+            variance = np.std(val)/(np.sqrt(len(val)))
 
             if term == 'Success':
                 variance *= 100
@@ -118,17 +116,28 @@ def parse_plot(files, term='Reward'):
         for epi_val in episode_coll[label]:
             mean_episodes.append(sum(epi_val) / len(epi_val))
 
-        plt.plot(np.arange(len(coll[label])), mean_values, linewidth=1.5, label=label, color=colors_map[label])
+        print(label)
+        print('max: ', np.max(mean_values))
+        print('min: ', np.min(mean_values))
+        max_idx = np.argmax(mean_values)
+        min_idx = np.argmin(mean_values)
+        print('max std: ', np.std(coll[label][max_idx]))
+        print('min std: ', np.std(coll[label][min_idx]))
+
+        plt.plot(np.arange(len(coll[label])), mean_values, linewidth=2.0, label=label, color=colors_map[label])
         plt.fill_between(np.arange(len(coll[label])), min_values, max_values, color=colors.to_rgba(colors_map[label], alpha=0.2))
 
         # plt.plot(mean_episodes, mean_values, linewidth=1.5, label=label, color=colors_map[label])
         # plt.fill_between(mean_episodes, min_values, max_values, color=colors.to_rgba(colors_map[label], alpha=0.2))
 
     plt.xlabel('Epochs')
+    if term == 'Success':
+        term = 'Success Rate (%)'
     plt.ylabel(term)
-    plt.legend()
+    # plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=3, mode="expand", borderaxespad=0.)
+    # plt.legend(framealpha=1)
     plt.grid()
-#   plt.title('GFootball {} {}'.format(sys.argv[2], term))
+#     plt.title('GFootball {} {}'.format(sys.argv[2], term))
 
 files = glob.glob(sys.argv[1] + "*")
 files = list(filter(lambda x: x.find(".pt") == -1, files))

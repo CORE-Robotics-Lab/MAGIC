b378f8cdd3ec19e621a29eaf549fbbbffd4826c8
Yaru Niu <chrisyrniu@gmail.com>
2021-04-19 08:27:55 -0400

    Delete grf-envs directory

--- grf-envs/grf_envs/__init__.py
+++
@@ -1,7 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='GRFWrapper-v0',
-    entry_point='grf_envs.grf_wrapper_env:GRFWrapperEnv',
-)
-
--- grf-envs/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-
-setup(name='grf_envs',
-      version='0.0.1',
-      install_requires=['gym','numpy', 'gfootball']  
-)  
